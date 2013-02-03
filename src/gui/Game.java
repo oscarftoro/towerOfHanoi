3ef5f05bc2bf61020a2457e658a9e672683bf484
OscarToro <groovenino@gmail.com>
2013-02-03 01:05:11 +0100

    Add towers

--- src/gui/Game.java
+++ src/gui/Game.java
@@ -9,7 +9,9 @@ import javafx.scene.image.ImageView;
 public class Game extends Parent {
 	private ImageView background;
 	private ImageView tower1;
-	//wdwdw
+	private ImageView tower2;
+	private ImageView tower3;
+	
 	Game() {
 		background = new ImageView();
 		background.setFocusTraversable(true);
@@ -20,15 +22,26 @@ public class Game extends Parent {
 		
 		tower1 = new ImageView();
 		tower1.setImage(Config.getImages().get(Config.IMAGE_TOWER));
-		tower1.setFitWidth(50);
-		tower1.setFitHeight(600);
-		tower1.setLayoutX(30);
-		tower1.setLayoutY(-30);
+		tower1.setFitWidth(15);
+		tower1.setFitHeight(321);
+		tower1.setLayoutX(130);
+		tower1.setLayoutY(80);
 		
+		tower2 = new ImageView();
+		tower2.setImage(Config.getImages().get(Config.IMAGE_TOWER));
+		tower2.setFitWidth(15);
+		tower2.setFitHeight(321);
+		tower2.setLayoutX(395);
+		tower2.setLayoutY(80);
 		
-		
+		tower3 = new ImageView();
+		tower3.setImage(Config.getImages().get(Config.IMAGE_TOWER));
+		tower3.setFitWidth(15);
+		tower3.setFitHeight(321);
+		tower3.setLayoutX(660);
+		tower3.setLayoutY(80);
 		Group group = new Group();
-		group.getChildren().addAll(background,tower1);
+		group.getChildren().addAll(background,tower1,tower2,tower3);
 		getChildren().add(group);
 	}
 
